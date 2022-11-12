dc12edf6fecaaf9c379d02537f301e2c0ed04a34
santosh10499 <115284357+santosh10499@users.noreply.github.com>
2022-11-12 12:03:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const colorBlack=document.getElementById("container");
+const text=document.getElementById("heading");
+function fun1(){
+   text.innerHTML="Background Color Palegoldenrod";
+colorBlack.style.backgroundColor="palegoldenrod";
+
+}
+
+function fun2(){
+    text.innerHTML="Background Color Palegreen";
+colorBlack.style.backgroundColor="palegreen";
+    
+}
+    function fun3(){
+    text.innerHTML="Background Color Orange";  
+    colorBlack.style.backgroundColor="orange";
+        
+    }
+    function fun4(){
+        text.innerHTML="Background Color Hotpink";
+        colorBlack.style.backgroundColor="hotpink";
+            
+        }
+        function fun5(){
+            text.innerHTML="Background Color Olivedrab";
+            colorBlack.style.backgroundColor="olivedrab";
+                
+        }
+            function fun6(){
+                text.innerHTML="Background Color Gold";
+                colorBlack.style.backgroundColor="gold";
+                    
+                }
+                function fun7(){
+                    text.innerHTML="Background Color Ghostwhite";
+                    colorBlack.style.backgroundColor="ghostwhite";
+                        
+             }
+             function fun8(){
+                text.innerHTML="Background Color Peru";
+                colorBlack.style.backgroundColor="peru";
+                    
+         }
+         function fun9(){
+            text.innerHTML="Background Color Gray";
+            colorBlack.style.backgroundColor="gray";
+                
+     }
+     function fun10(){
+        text.innerHTML="Background Color Teal";
+        colorBlack.style.backgroundColor="teal";
+            
+ }
+             
+    
